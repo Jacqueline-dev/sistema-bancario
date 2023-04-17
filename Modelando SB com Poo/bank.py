43312b92b0562e86f037b87b4e9e1e00003f216c
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2023-04-17 15:26:53 -0300

    classe sacar e depositar

--- Modelando SB com Poo/bank.py
+++ Modelando SB com Poo/bank.py
@@ -1,16 +1,18 @@
 from abc import ABC, abstractclassmethod, abstractclassmethod, abstractproperty
 from datetime import datetime
 
+
 class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
         self.contas = []
-    
+
     def realizar_transacao(self, conta, transacao):
         transacao.registrar(conta)
-    
+
     def adicionar_conta_transacao(self, conta):
         self.contas.append(conta)
+
 
 class PessoaFisica(Cliente):
     def __init__(self, nome, data_nascimento, cpf, endereco):
@@ -19,4 +21,62 @@ class PessoaFisica(Cliente):
         self.data_nascimento = data_nascimento
         self.cpf = cpf
 
-        
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
+
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls(numero, cliente)
+
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @property
+    def numero(self):
+        return self._numero
+
+    @property
+    def agencia(self):
+        return self._agencia
+
+    @property
+    def cliente(self):
+        return self._cliente
+
+    @property
+    def historico(self):
+        return self._historico
+
+
+def sacar(self, valor):
+    saldo = self.saldo
+    excedeu_saldo = valor > saldo
+
+    if excedeu_saldo:
+        print("\n@@@ Operação falhou! Voce não tem saldo suficiente. @@@")
+
+    elif valor > 0:
+        self._saldo -= valor
+        print("\n=== Saque realizado com sucesso! ===")
+        return True
+
+    else:
+        print("\n@@@ Opração falhou! O valor informado é inválido. @@@")
+    return False
+
+def depositar(self, valor):
+    if valor > 0:
+        self._saldo += valor
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        return 
+    
+    return True
