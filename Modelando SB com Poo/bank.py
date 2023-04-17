4c6e9ab0c5d221afb19d579d3eee1fe6a82d1760
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2023-04-17 15:02:29 -0300

    criação da classe cliente e pessoa fisica

---
+++ Modelando SB com Poo/bank.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractclassmethod, abstractclassmethod, abstractproperty
+from datetime import datetime
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+    
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+    
+    def adicionar_conta_transacao(self, conta):
+        self.contas.append(conta)
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+
+        
